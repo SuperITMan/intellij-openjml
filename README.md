91035696f469ab642438d47502395dce30c11a39
Wang Suyuchen <jim_wangsu@126.com>
2019-04-27 21:33:15 +0800

    added IntelliJ 2019.1 support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # OpenJML Plugin for IntelliJ IDEA  
 This Plugin provides the OpenJML/ESC to IntelliJ IDEA. 
 
+Note: The official plugin only support IntelliJ IDEA 2018.2 and lower. This version is recompiled and changed a bit by Sheryc, to support IntelliJ IDEA 2019.1.
+
 ## Getting started
 
 ### Installtion
@@ -15,4 +17,3 @@ After you successfully installed the plugin you have to choose how you want to g
 To use the plugin you have to configure it. Go to the intellij settings an choose the OpenJML tab. 
 1. Set the path to the openjml commandline tool if you had downloaded it manually. Otherwise click the download button and choose a directory where to download the openjml commanline tool.
 2. Choose a solver which is shipped with the commandline tool or set the path to a custom solver.
-
--- src/main/java/fh/luebeck/openjml/actions/JmlRunAction.java
+++ src/main/java/fh/luebeck/openjml/actions/JmlRunAction.java
@@ -1,22 +1,20 @@
 package fh.luebeck.openjml.actions;
 
 import com.intellij.execution.ExecutionException;
-import com.intellij.ide.SaveAndSyncHandler;
 import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
 import fh.luebeck.openjml.util.JmlRunUtil;
 
 public class JmlRunAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        SaveAndSyncHandler.getInstance().saveProjectsAndDocuments();
+        FileDocumentManager.getInstance().saveAllDocuments();
         VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
         if(virtualFile != null) {
             String pathToJavaFile = virtualFile.getCanonicalPath();
